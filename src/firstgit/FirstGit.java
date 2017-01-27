deb6116b7b898bee3a6e8c553d6552a33dbcfa79
msanders11 <msanders11@L11702042765.wctc.msft>
2017-01-26 20:52:28 -0600

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,16 @@
+package firstgit;
+
+/**
+ *
+ * @author msanders11
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
